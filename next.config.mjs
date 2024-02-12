6668c1c4c69e6052cd6134047244816433f08303
Dale Rogers <hello@dalerogers.com.au>
2024-02-12 16:28:57 +1100

    Update next.config.mjs
    encountered an export error preventing deployment. This should fix it.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Add the output property to specify the export directory
+  output: {
+    // Specify the export directory
+    dir: 'out', // Change 'out' to your desired export directory
+  },
+};
 
 export default nextConfig;
