7185f39aef56ba8c159a75366a5207016ffa3408
Matthew Altenburg <mattaltenburg@gmail.com>
2024-02-13 14:46:20 +1100

    Revert "Update next.config.mjs"
    This reverts commit 6668c1c4c69e6052cd6134047244816433f08303.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Add the output property to specify the export directory
-  output: {
-    // Specify the export directory
-    dir: 'out', // Change 'out' to your desired export directory
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
