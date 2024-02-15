8078b058bbd3cd1dba5e7d7f89e80e56440fe5a8
Matthew Altenburg <mattaltenburg@gmail.com>
2024-02-15 15:40:27 +1100

    added a temp but better landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="emerald">
       <body className={inter.className}>
         {/* <>Navbar</> */}
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
 export default function Home() {
   return (
-    <h1 className="text-5xl font-bold z-10 text-primary">
-      Coming soon Southern Cross AI
-    </h1>
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: "url('/logo.webp')",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Southern Cross AI</h1>
+          <p className="mb-5">
+            We are a research initiative aimed at developing Australia's first
+            open-source Large Language Model (LLM). Our project aims brings
+            together a diverse team of experts from academia, research,
+            government, and business sectors, united by a shared vision of
+            advancing AI technology in Australia.
+          </p>
+          {/* <button className="btn btn-primary">Get Started</button> */}
+        </div>
+      </div>
+    </div>
   );
 }
