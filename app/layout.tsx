b79ce1a19fe8882f3d9ee2a4aafe40ba819bd760
Matthew Altenburg <mattaltenburg@gmail.com>
2024-02-28 07:08:01 +1100

    changed theme to blue aqua

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="winter">
+    <html lang="en" data-theme="aqua">
       <body className={`${inter.className} `}>
         <Navbar />
         {children}
