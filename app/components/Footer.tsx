59d9000a9019de5d5d68af460bf743afab6c3a26
mamtagrewal <mamtasagrewal@gmail.com>
2024-03-20 09:22:40 +1100

    Removed press kit and jobs from footer.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
       <nav className="grid grid-flow-col gap-4">
         <a className="link link-hover">About us</a>
         <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
+
+
       </nav>
       <nav>
         <div className="grid grid-flow-col gap-4">
