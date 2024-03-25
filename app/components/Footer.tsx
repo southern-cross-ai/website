78a856cfa930a8d74cd975786ae16330eea76d5e
boya wang <boya.wang@anu.edu.au>
2024-03-25 18:51:21 +1100

    link footer page

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
       <nav className="grid grid-flow-col gap-4">
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
+        <Link href="../about" className="link link-hover">
+          About us
+        </Link>
+
+        <Link href="../calendar" className="link link-hover">
+          Contact
+        </Link>
+
         <a className="link link-hover">Jobs</a>
         <a className="link link-hover">Press kit</a>
       </nav>
